c50c8487d230dbaddaa3348b5504d380d71a99c6
Tim Osborn <tim@memelab.com.au>
2025-02-24 00:09:04 +1100

    feat: link Banner to /

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Banner = ({ title }: { title: string }) => {
   return (
-    <div className="Banner flex flex-col justify-center items-center">
-      <Image src="/images/evolve-logo.jpg" alt="Evolve Logo" width={200} height={200} />
-      <h1 className="text-5xl font-extrabold text-neutral-700 dark:text-neutral-400 tracking-tight mt-6 mb-2">
-        {title}
-      </h1>
+    <div className="Banner">
+      <Link href="/" className="flex flex-col justify-center items-center">
+        <Image src="/images/evolve-logo.jpg" alt="Evolve Logo" width={200} height={200} />
+        <h1 className="text-5xl font-extrabold text-neutral-700 dark:text-neutral-400 tracking-tight mt-6 mb-2">
+          {title}
+        </h1>
+      </Link>
     </div>
   )
 }
