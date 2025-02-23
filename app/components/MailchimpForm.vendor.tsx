c9b41f21d3ff967605d59ab34951fa566d796863
Tim Osborn <tim@memelab.com.au>
2025-02-24 00:19:53 +1100

    fix: dont dedent mailchimp... fix it later

--- app/components/MailchimpForm.vendor.tsx
+++ app/components/MailchimpForm.vendor.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script'
 export const MailChimpVendorForm = () => {
   return (
     // Hack: dedent, cos MC form is remotely styled for now
-    <div className="-mx-[20px]">
+    <div className="MailchimpForm">
       <div dangerouslySetInnerHTML={{ __html: mailchimpMarkup }} />
       <Script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js" strategy="beforeInteractive" />
       <Script
