1371da873abcfc23712b15748127e560c4ee93c2
Tim Osborn <tim@memelab.com.au>
2025-02-23 22:10:51 +1100

    feat: render banner image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CustomMDX } from './components/CustomMDX'
 import { getMDXData } from './lib/utils'
 import path from 'path'
+import Image from 'next/image'
 
 export default function Page() {
   let pages = getMDXData(path.join(process.cwd(), 'app'))
@@ -8,6 +9,10 @@ export default function Page() {
 
   return (
     <section className="prose">
+      <div className="banner flex justify-center items-center">
+        <Image src="/images/evolve-logo.jpg" alt="Evolve Logo" width={200} height={200} />
+      </div>
+
       <h1>Evolve Community</h1>
 
       <CustomMDX source={homepage.content} />
