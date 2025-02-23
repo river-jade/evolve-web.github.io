31e0aaf3aaaecdcb9ae38ad3947aed2e4e245168
Tim Osborn <tim@memelab.com.au>
2025-02-23 15:45:57 +1100

    feat: render Poppins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { Poppins } from 'next/font/google'
 import { Navbar } from './components/Navbar'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/Footer'
 import { baseUrl } from './sitemap'
 import { cx } from './lib/cx'
+
+// https://nextjs.org/docs/pages/building-your-application/optimizing/fonts#google-fonts
+// https://fonts.google.com/specimen/Poppins
+const poppinsFont = Poppins({ subsets: ['latin'], weight: '400' })
 
 const title = 'Evolve Community'
 const description = `Evolve is a "transformational" festival; a gathering of people on
@@ -44,10 +47,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={cx('text-black bg-white dark:text-white dark:bg-black', GeistSans.variable, GeistMono.variable)}
-    >
+    <html lang="en" className={cx('text-black bg-white dark:text-white dark:bg-black', poppinsFont.className)}>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
