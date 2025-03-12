8a4eb427988a109dd95ecb48870608a0a8bd1d8d
River Satya <river.satya@gmail.com>
2025-03-12 15:10:02 +1100

    Add redirect for mailing list signup

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
         destination: 'https://linktr.ee/evolve.community',
         permanent: true,
       },
+      {
+        source: '/mailing-list',
+        destination: 'https://world.us10.list-manage.com/subscribe?u=2f59f4888aeaef053a48ad2bc&id=100146c012',
+        permanent: true,
+      },
     ]
   },
 }
