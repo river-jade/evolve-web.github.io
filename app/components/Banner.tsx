74ef64f8455e86f05b31c933f9d59d689bcf64f7
River Satya <river.satya@gmail.com>
2026-01-10 20:23:56 +1000

    Remove logo from banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -4,17 +4,11 @@ import Link from 'next/link'
 export const Banner = ({ title }: { title: string }) => {
   return (
     <div className="Banner">
-      <Link href="/" className="flex flex-col justify-center items-center">
-        <Image
-          src="/images/evolve-logo.jpg"
-          alt="Evolve Logo"
-          width={200}
-          height={200}
-        />
+      <div className="flex flex-col justify-center items-center">
         <h1 className="text-5xl font-extrabold text-neutral-700 tracking-tight mt-6 mb-2">
           {title}
         </h1>
-      </Link>
+      </div>
     </div>
   )
 }
